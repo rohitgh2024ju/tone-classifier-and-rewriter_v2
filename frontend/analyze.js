28b2709f68a0dbcda99f58d0030780d110bc186c
rohitgh2024ju <rohitstudying2004@gmail.com>
2026-03-08 14:29:23 +0530

    polish

--- frontend/analyze.js
+++ frontend/analyze.js
@@ -36,7 +36,7 @@ async function analyzeText() {
   const optionElementValue = optionElement.value;
 
   try {
-    const response = await fetch("http://127.0.0.1:8000/predict", {
+    const response = await fetch("https://politicly-winier-dona.ngrok-free.dev/predict", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
